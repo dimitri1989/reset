38e3b09b4d6d4422d5b60050d1e7f052e2b5fe34
dimitri1989 <dimitri.kvarelashvili@gmail.com>
2023-05-07 23:44:16 +0400

    Add files via upload

---
+++ input.js
@@ -0,0 +1,7 @@
+function dadastureba(){
+    var Emeil = document.getElementById("email").value;
+    var pasword = document.getElementById("pasword1").value;
+    document.getElementById("reset").reset();
+return false;
+};
+
